a1b4c52bf7ace9512a9a1af3538512c77ccac136
Jixiee <write2jigs24@gmail.com>
2024-05-28 11:19:32 +0530

    Changed Hello World to star pattern

--- lib/hello_world_web/controllers/page_controller.ex
+++ lib/hello_world_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule HelloWorldWeb.PageController do
   use HelloWorldWeb, :controller
 
   def index(conn, _params) do
-    text conn, "Hello World"
+    star_pattern = generate_star_pattern(5)
+    text conn, star_pattern
+  end
+
+  defp generate_star_pattern(n) do
+    1..n
+    |> Enum.map(fn i -> String.duplicate("*", i) end)
+    |> Enum.join("\n")
   end
 end
--- lib/hello_world_web/router.ex
+++ lib/hello_world_web/router.ex
@@ -5,7 +5,7 @@ defmodule HelloWorldWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {HelloWorldWeb.Layouts, :root}
+    plug :put_root_layout, {HelloWorldWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
@@ -18,27 +18,5 @@ defmodule HelloWorldWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", HelloWorldWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:hello_world, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: HelloWorldWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
   end
 end
